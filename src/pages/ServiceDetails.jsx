d31ed25d6c04eed10d1ed5e924b389f559ac3616
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-09-28 13:42:56 -0700

    starting user section

---
+++ src/pages/ServiceDetails.jsx
@@ -0,0 +1,27 @@
+// Service Information:
+
+// Title
+// Description
+// Service Type
+// Period
+// Approved
+// Price
+// Category
+// Usage Type
+// Created At
+// Last Updated
+// User and Interaction Information:
+// 11. User
+
+// Likes Status
+// Saves
+// Rejected
+// Discount
+// Views
+// Service Management:
+// 17. Initial Deposit
+
+// Deleted
+// Delivery
+// Metadata:
+// 20. Meta
